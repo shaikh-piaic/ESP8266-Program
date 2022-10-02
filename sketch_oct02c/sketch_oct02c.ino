0262e5257b12a020facd6a973848d18723d778fc
shaikh-piaic <108601988+shaikh-piaic@users.noreply.github.com>
2022-10-02 11:58:04 +0500

    Mac Address code

---
+++ sketch_oct02c/sketch_oct02c.ino
@@ -0,0 +1,15 @@
+#include <ESP8266WiFi.h>
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println();
+  Serial.print("ESP8266 Mac Address: ");
+  Serial.println(WiFi.macAddress());
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
